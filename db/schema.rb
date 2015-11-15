00e2e9b379a443f9b90623335bcb011a16ce52a0
Vlad Marinov <Vlad@Vlads-MBP.local>
2015-11-14 21:01:51 -0800

    added updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115043636) do
+ActiveRecord::Schema.define(version: 20151115045619) do
 
   create_table "carriers", force: :cascade do |t|
     t.integer  "dot_number"
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(version: 20151115043636) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

777c3810865767ce7d6291c21d35fb2918dd4b14
Vlad Marinov <vladmrnv@gmail.com>
2015-11-14 16:36:18 -0800

    Made basic user model stuff with login/logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113050415) do
+ActiveRecord::Schema.define(version: 20151115002259) do
 
   create_table "carriers", force: :cascade do |t|
     t.integer  "dot_number"
@@ -168,5 +168,23 @@ ActiveRecord::Schema.define(version: 20151113050415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "vendors", ["email"], name: "index_vendors_on_email", unique: true
+  add_index "vendors", ["reset_password_token"], name: "index_vendors_on_reset_password_token", unique: true
 
 end
